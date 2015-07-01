d343cc03a0f650176570a42a7c4513b8f2b8a273
Hackbright Student <info@hackbrightacademy.com>
2015-07-01 16:22:34 -0700

    works and lets you try again

--- calculator.py
+++ calculator.py
@@ -12,32 +12,46 @@ from arithmetic import *
 def simple_calculator():
     input_math = str(raw_input("What is your math?"))
     token = input_math.rstrip()
-    token = token.split(" ")
+    token = input_math.split(" ")
+    token[1] = int(token[1])
+    token[2] = int(token[2])
+    print token
     while True:
-        if token[0] == "q" or "Q":
+        if token[0] == "q" or token[0] == "Q":
+            print type(token[0])
+            print token[0]
             print "Goodbye!"
             return 
         else:
             if token[0] == "add":
                 print add(token[1], token[2])
-            elif token[0] == "sub":
+                return
+            elif token[0] == "subtract":
                 print subtract(token[1], token[2])
+                return
             elif token[0] == "multiply":
                 print multiply(token[1], token[2])
+                return
             elif token[0] == "divide":
                 print divide(token[1], token[2])
+                return
             elif token[0] == "square":
                 print square(token[1], token[2])
+                return
             elif token[0] == "cube":
                 print cube(token[1])
+                return
             elif token[0] == "power":
                 print power(token[1], token[2])
+                return
             elif token[0] == "mod":
                 print mod(token[1], token[2])
+                return
             else:
                 print "That is not a correct input"
+                simple_calculator()
 
-                
+simple_calculator()
 
 
 
