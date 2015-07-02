18f70a9171e997c847f1a7f9b0e3c67ef5d55150
Hackbright Student <info@hackbrightacademy.com>
2015-07-02 13:01:07 -0700

    works with different integers

--- calculator.py
+++ calculator.py
@@ -16,42 +16,52 @@ def quit_program():
         simple_calculator()
     else:
         print "That is not an answer, MAKE UP YOUR MIND"
+        quit_program()
 
 # Your code goes here
 def simple_calculator():
     formula_input = str(raw_input("Please enter Formula: "))
-    interger_input_1 = int(raw_input("Please Enter First Number: "))
-    interger_input_2 = int(raw_input("Please Enter Second Number If Applicable: "))
+    one_integers_needed_list = ("square", "^2","cube", "^3")
+
+    for function_type in one_integers_needed_list:
+        if formula_input not in function_type:
+            integer_input_1 = int(raw_input("Please Enter First Number: "))
+            print "i'm printing one number"
+            break
+        else:
+            integer_input_1 = int(raw_input("Please Enter First Number: "))
+            integer_input_2 = int(raw_input("Please Enter Second Number: "))
+            print "i'm printing two number"
+            break
 
     while True:
-            if formula_input == "add":
-                print add(interger_input_1, interger_input_2)
+            if formula_input == "add" or formula_input == "+":
+                print add(integer_input_1, integer_input_2)
                 break
-            elif formula_input == "subtract":
-                print subtract(interger_input_1, interger_input_2)
+            elif formula_input == "subtract" or formula_input == "-":
+                print subtract(integer_input_1, integer_input_2)
                 break
-            elif formula_input == "multiply":
-                print multiply(interger_input_1, interger_input_2)
+            elif formula_input == "multiply" or formula_input == "*":
+                print multiply(integer_input_1, integer_input_2)
                 break
-            elif formula_input == "divide":
-                print divide(interger_input_1, interger_input_2)
+            elif formula_input == "divide" or formula_input == "/":
+                print divide(integer_input_1, integer_input_2)
                 break
-            elif formula_input == "square":
-                print square(interger_input_1, interger_input_2)
+            elif formula_input == "square" or formula_input == "^2":
+                print square(integer_input_1)
                 break
-            elif formula_input == "cube":
-                print cube(interger_input_1, interger_input_2 == None)
+            elif formula_input == "cube" or formula_input == "^3":
+                print cube(integer_input_1)
                 break
-            elif formula_input == "power":
-                print power(interger_input_1, interger_input_2)
+            elif formula_input == "power" or formula_input == "^":
+                print power(integer_input_1, integer_input_2)
                 break
-            elif formula_input == "module":
-                print mod(interger_input_1, interger_input_2)
+            elif formula_input == "module" or formula_input == "%":
+                print mod(integer_input_1, integer_input_2)
                 break
             else:
                 print "That is not a correct input"
                 break
-    print "Awesome! Go Again! Type 'q' to quit"
     quit_program()
 
 simple_calculator()
